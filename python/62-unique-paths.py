b09325283b71feaa67df9dc420b588b21414ae69
Devdhar Manpuria <btech10748.23@bitmesra.ac.in>
2025-06-24 12:17:33 +0530

    Renaming

--- python/62-unique-paths.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        above_row = [1] * n
-        for row in range(1, m):
-            current_row = [1] * n
-            for col in range(1, n):
-                current_row[col] = current_row[col - 1] + above_row[col]
-            above_row = current_row
-        return above_row[n - 1]
