2dce7047e0c257c115a3398696b44423dd2ee71e
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-01-26 18:06:13 +0530

    59 ms (44.44%) 1200-minimum-absolute-difference .py

---
+++ LeetCode/Easy/1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        n = len(arr)
+        min_diff = arr[1] - arr[0]
+        for i in range(n - 1):
+            min_diff = min(min_diff, arr[i + 1] - arr[i])
+        sol = []
+        for i in range(n - 1):
+            if arr[i + 1] - arr[i] == min_diff:
+                sol.append([arr[i], arr[i + 1]])
+        return sol
