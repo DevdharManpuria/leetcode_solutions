ac85774e37c6ba426585514ba936c1461ac6a93b
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-05-18 16:35:16 +0530

    8 ms (97.15%) 0739-daily-temperatures .cpp

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> res(temperatures.size(), 0);
-        stack<pair<int, int>> stack; 
-        for (int i = 0; i < temperatures.size(); i++) {
-            int t = temperatures[i];
-            while (!stack.empty() && t > stack.top().first) {
-                auto pair = stack.top();
-                stack.pop();
-                res[pair.second] = i - pair.second;
+        int n = temperatures.size();
+        vector<int> res(n, 0);
+        for (int i = n - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (res[j] == 0) {
+                    j = n;
+                    break;
+                }
+                j += res[j];
             }
-            stack.push({t, i});
+            if (j < n)
+                res[i] = j - i;
         }
         return res;
     }
