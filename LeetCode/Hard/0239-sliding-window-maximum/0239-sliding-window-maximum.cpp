7bf34051dddb00399a3d3289d55268027426e352
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-06-04 01:48:45 +0530

    26 ms (46%) 0239-sliding-window-maximum .cpp

--- LeetCode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ LeetCode/Hard/0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,16 +5,14 @@ public:
         vector<int> output(n - k + 1);
         deque<int> q;
         int l = 0, r = 0;
-        while (r < n) {
-            while (!q.empty() && nums[q.back()] < nums[r]) {
+        while(r<n){
+            while(!q.empty() && nums[q.back()]<nums[r])
                 q.pop_back();
-            }
             q.push_back(r);
-            if (l > q.front()) {
+            if(l>q.front())
                 q.pop_front();
-            }
-            if ((r + 1) >= k) {
-                output[l] = nums[q.front()];
+            if((r+1)>=k){
+                output[l]=nums[q.front()];
                 l++;
             }
             r++;
