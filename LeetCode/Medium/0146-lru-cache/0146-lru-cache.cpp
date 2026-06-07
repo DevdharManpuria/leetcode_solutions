6b696ed8cd89a2d151c789324c49faee76c2ca33
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-06-07 22:28:02 +0530

    70 ms (68.59%) 0146-lru-cache .cpp

--- LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.cpp
@@ -1,31 +1,68 @@
+class Node {
+public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int k, int v) : key(k), val(v), prev(nullptr), next(nullptr) {}
+};
+
 class LRUCache {
 private:
-    unordered_map<int, pair<int, list<int>::iterator>> cache;
-    list<int> order;
-    int capacity;
+    int cap;
+    unordered_map<int, Node*> cache;
+    Node* left;
+    Node* right;
+
+    void remove(Node* node) {
+        Node* prev = node->prev;
+        Node* nxt = node->next;
+        prev->next = nxt;
+        nxt->prev = prev;
+    }
+
+    void insert(Node* node) {
+        Node* prev = right->prev;
+        prev->next = node;
+        node->prev = prev;
+        node->next = right;
+        right->prev = node;
+    }
 
 public:
     LRUCache(int capacity) {
-        this->capacity = capacity;
+        cap = capacity;
+        cache.clear();
+        left = new Node(0, 0);
+        right = new Node(0, 0);
+        left->next = right;
+        right->prev = left;
     }
 
     int get(int key) {
-        if (cache.find(key) == cache.end()) return -1;
-        order.erase(cache[key].second);
-        order.push_back(key);
-        cache[key].second = --order.end();
-        return cache[key].first;
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            remove(node);
+            insert(node);
+            return node->val;
+        }
+        return -1;
     }
 
     void put(int key, int value) {
         if (cache.find(key) != cache.end()) {
-            order.erase(cache[key].second);
-        } else if (cache.size() == capacity) {
-            int lru = order.front();
-            order.pop_front();
-            cache.erase(lru);
+            remove(cache[key]);
         }
-        order.push_back(key);
-        cache[key] = {value, --order.end()};
+        Node* newNode = new Node(key, value);
+        cache[key] = newNode;
+        insert(newNode);
+
+        if (cache.size() > cap) {
+            Node* lru = left->next;
+            remove(lru);
+            cache.erase(lru->key);
+            delete lru;
+        }
     }
 };
