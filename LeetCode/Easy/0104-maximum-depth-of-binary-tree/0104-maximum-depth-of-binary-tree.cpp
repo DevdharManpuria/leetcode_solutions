c68dd57899f632c93eee426120b8ce2d709098a7
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-06-11 15:45:06 +0530

    0 ms (100%) 0104-maximum-depth-of-binary-tree .cpp

--- LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,7 +12,22 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == nullptr) return 0;
-        return 1+max(maxDepth(root->left),maxDepth(root->right));
+        stack<pair<TreeNode*, int>> stack;
+        stack.push({root, 1});
+        int res = 0;
+
+        while (!stack.empty()) {
+            pair<TreeNode*, int> current = stack.top();
+            stack.pop();
+            TreeNode* node = current.first;
+            int depth = current.second;
+
+            if (node != nullptr) {
+                res = max(res, depth);
+                stack.push({node->left, depth + 1});
+                stack.push({node->right, depth + 1});
+            }
+        }
+        return res;
     }
 };
