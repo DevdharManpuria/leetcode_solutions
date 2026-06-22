bf411e6b93dbe2a8d6985b6cc3631aea0549afb9
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-06-22 18:42:13 +0530

    0 ms (100%) 0198-house-robber .cpp

--- LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        vector<int> dp(n);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        int rob1 = 0, rob2 = 0;
+        for (int num : nums) {
+            int temp = max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
         }
-        return dp[n-1];
+        return rob2;
     }
 };
