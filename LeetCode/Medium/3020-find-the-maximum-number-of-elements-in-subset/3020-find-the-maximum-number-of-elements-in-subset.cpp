50096437e8019106773a8dc93cbcfee02a427399
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-06-27 13:27:19 +0530

    88 ms (62.86%) 3020-find-the-maximum-number-of-elements-in-subset .cpp

--- LeetCode/Medium/3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ LeetCode/Medium/3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        map<long long, int> hm;
-        int max_len = 1;
-        for(int i : nums)
-            hm[i]++;
-        for(int i : nums) {
-            if(i == 1) {
-                int len = hm[1];
-                if(len % 2 == 0) len--;
-                max_len = max(max_len, len);
-                continue;
-            }
-            long long curr = i;
+        unordered_map<long long, int> hm;
+        for (int i : nums) hm[i]++;
+        int max_len = hm.count(1) ? hm[1] - (hm[1] % 2 == 0) : 1;
+        hm.erase(1); 
+        for (auto const& [key, val] : hm) {
+            long long curr = key;
             int curr_len = 0;
-            for(int j=0; j<=9; j++) {
-                if(hm.find(curr) != hm.end() && hm[curr] >= 2) {
-                    curr_len += 2;
-                    curr = curr * curr;
-                } 
-                else if(hm.find(curr) != hm.end() && hm[curr] > 0) {
-                    curr_len += 1;
-                    break;
-                } 
-                else {
-                    curr_len -= 1;
-                    break;
-                }
+            while (hm.count(curr) && hm[curr] >= 2) {
+                curr_len += 2;
+                curr *= curr;
             }
-            max_len = max(max_len, curr_len);
+            max_len = max(max_len, curr_len + (hm.count(curr) ? 1 : -1));
         }
         return max(1, max_len);
     }
