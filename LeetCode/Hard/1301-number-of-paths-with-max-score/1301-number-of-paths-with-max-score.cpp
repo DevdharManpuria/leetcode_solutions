a0107d695d5abc22576d070b28b56df86ddcca4d
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-06 01:25:00 +0530

    9 ms (60.4%) 1301-number-of-paths-with-max-score .cpp

--- LeetCode/Hard/1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ LeetCode/Hard/1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
-        int mod = 1e9 + 7;
         int n = board.size();
-        vector<vector<int>> maxScore(n, vector<int>(n, -1));
-        vector<vector<int>> ways(n, vector<int>(n, 0));
-        maxScore[n - 1][n - 1] = 0;
-        ways[n - 1][n - 1] = 1;
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = n - 1; j >= 0; j--){
-                if(board[i][j] == 'X'){
-                    continue;
-                }
-                if(i == n - 1 && j == n - 1){
-                    continue;
-                }
-                int bestScore = -1;
-                long long totalWays = 0;
-                if(i + 1 < n && maxScore[i + 1][j] != -1){
-                    if(maxScore[i + 1][j] > bestScore){
-                        bestScore = maxScore[i + 1][j];
-                        totalWays = ways[i + 1][j];
-                    }else if(maxScore[i + 1][j] == bestScore){
-                        totalWays = (totalWays + ways[i + 1][j]) % mod;
-                    }
-                }
-                if(j + 1 < n && maxScore[i][j + 1] != -1){
-                    if(maxScore[i][j + 1] > bestScore){
-                        bestScore = maxScore[i][j + 1];
-                        totalWays = ways[i][j + 1];
-                    }else if(maxScore[i][j + 1] == bestScore){
-                        totalWays = (totalWays + ways[i][j + 1]) % mod;
-                    }
-                }
-                if(i + 1 < n && j + 1 < n && maxScore[i + 1][j + 1] != -1){
-                    if(maxScore[i + 1][j + 1] > bestScore){
-                        bestScore = maxScore[i + 1][j + 1];
-                        totalWays = ways[i + 1][j + 1];
-                    }else if(maxScore[i + 1][j + 1] == bestScore){
-                        totalWays = (totalWays + ways[i + 1][j + 1]) % mod;
-                    }
-                }
-                if(bestScore == -1){
-                    continue;
-                }
-                int curr = 0;
-                if(board[i][j] >= '1' && board[i][j] <= '9'){
-                    curr = board[i][j] - '0';
-                }
-                maxScore[i][j] = bestScore + curr;
-                ways[i][j] = totalWays % mod;
+        const int MOD = 1e9 + 7;
+        vector<pair<int,int>> below(n, {-1, 0});
+        vector<pair<int,int>> cur  (n, {-1, 0}); 
+        for (int i = n - 1; i >= 0; --i) {
+            fill(cur.begin(), cur.end(), make_pair(-1, 0));   
+            for (int j = n - 1; j >= 0; --j) {
+                if (i == n - 1 && j == n - 1) { cur[j] = {0, 1}; continue; } // S
+                if (board[i][j] == 'X') continue;
+                int best = -1;
+                if (i + 1 < n)              best = max(best, below[j].first);
+                if (j + 1 < n)              best = max(best, cur[j + 1].first);
+                if (i + 1 < n && j + 1 < n) best = max(best, below[j + 1].first);
+                if (best == -1) continue;
+                long long cnt = 0;
+                if (i + 1 < n              && below[j].first     == best) cnt += below[j].second;
+                if (j + 1 < n              && cur[j + 1].first   == best) cnt += cur[j + 1].second;
+                if (i + 1 < n && j + 1 < n && below[j + 1].first == best) cnt += below[j + 1].second;
+                cnt %= MOD;
+                int val = (board[i][j] == 'E') ? 0 : board[i][j] - '0';
+                cur[j] = {best + val, (int)cnt};
             }
+            below = cur; 
         }
-        if(maxScore[0][0] == -1){
-            return {0, 0};
-        }
-        return {maxScore[0][0], ways[0][0]};
+        if (below[0].first == -1) return {0, 0};
+        return {below[0].first, below[0].second};
     }
 };
