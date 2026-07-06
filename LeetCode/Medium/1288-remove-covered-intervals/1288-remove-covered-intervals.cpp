af0d2991da24198e7c8c7e26defd1c15807b90eb
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-06 12:40:38 +0530

    0 ms (100%) 1288-remove-covered-intervals .cpp

--- LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& it) {
-        sort(it.begin(), it.end(), [](const vector<int>& a, const vector<int>& b) {
-            if (a[0] == b[0]) {
-                return a[1] > b[1];
-            }
-            return a[0] < b[0];
+    int removeCoveredIntervals(vector<vector<int>>& A) {
+        ranges::sort(A, {}, [](auto& x) {
+            return pair{x[0], -x[1]};
         });
-        int ans=0;
-        int me=0;
-        for(int i=0;i<it.size();i++){
-            if(it[i][1]>me){
-                me=it[i][1];
-                ans++;
-            }
+        int res = 0, r = 0;
+        for (auto& x : A) {
+            res += x[1] > r;
+            r = max(r, x[1]);
         }
-        return ans;
+        return res;
     }
 };
