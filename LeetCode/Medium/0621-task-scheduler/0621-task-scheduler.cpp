ffbb77da1e045369ac600d1efbdfd3a14ba79fde
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-12 03:45:28 +0530

    0 ms (100%) 0621-task-scheduler .cpp

--- LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.cpp
+++ LeetCode/Medium/0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> count(26, 0);
+        vector<int> count(26, 0); //count of each char
         for (char task : tasks) 
             count[task - 'A']++;
-        sort(count.begin(), count.end());
-        int maxf = count[25];
-        int idle = (maxf - 1) * n;
+        sort(count.begin(), count.end()); // sort to get the char with max occurences
+        int maxf = count[25]; // max frequency char count = maxf
+        int idle = (maxf - 1) * n; // amount of idle time we have (maxf - 1) gaps as maxf elements placed and each gap has n spaces
         for (int i = 24; i >= 0; i--) 
-            idle -= min(maxf - 1, count[i]);
-        return max(0, idle) + tasks.size();
+            idle -= min(maxf - 1, count[i]); // reduce count of each char from the idle time
+        return max(0, idle) + tasks.size(); // return 1 unit of time for each task and then any extra time need (if idle positive)
     }
 };
