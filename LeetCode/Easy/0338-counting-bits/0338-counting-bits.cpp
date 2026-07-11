e7f5b1a04ba5f65eb1ea0b40fcfc7ed2a66b7a15
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-12 04:14:15 +0530

    0 ms (100%) 0338-counting-bits .cpp

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> dp(n + 1, 0);
-        int sub = 1;
-        for (int i = 1; i <= n; i++) {
-            if (sub * 2 == i) 
-                sub = i;
-            dp[i] = dp[i - sub] + 1;
-        }
-        return dp;     
+       vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++) 
+            ans[i] = ans[i >> 1] + (i & 1); 
+        return ans;
     }
 };
