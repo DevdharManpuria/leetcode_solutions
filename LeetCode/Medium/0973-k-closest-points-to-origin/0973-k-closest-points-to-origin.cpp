e064f61e3dd8ab4ffb29a990aeaf5cf5d18d44b5
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-12 03:23:40 +0530

    74 ms (40.48%) 0973-k-closest-points-to-origin .cpp

---
+++ LeetCode/Medium/0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int,vector<int>>> pq;
+        for(auto& it: points){
+            int sq = it[0]*it[0] + it[1] * it[1]; 
+            pq.push({sq,it});
+            if(pq.size() > k) pq.pop();
+        }
+        vector<vector<int>> ans;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
