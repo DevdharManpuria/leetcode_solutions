c59434128187cc582b28ea0ef6112eba53c4a2d0
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-12 15:44:14 +0530

    0 ms (100%) 0371-sum-of-two-integers .cpp

--- LeetCode/Medium/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ LeetCode/Medium/0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        if (b==0) return a;
-        int carry=(a&b)<<1;
-        return getSum((a^b),carry);
+        while(b!=0){
+            int carry = (a & b) << 1;
+            a ^= b;
+            b = carry;
+        }
+        return a;
     }
 };
