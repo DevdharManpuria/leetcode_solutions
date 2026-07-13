d1abc339d20d7c1ad6615722a2ce4c3d38de94cd
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-13 12:20:56 +0530

    0 ms (100%) 1291-sequential-digits .cpp

--- LeetCode/Medium/1291-sequential-digits/1291-sequential-digits.cpp
+++ LeetCode/Medium/1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
+    inline static int q[45];
+    inline static bool init = []() {
+        int n = 0;
+        for (int i = 1; i < 10; i++)
+            q[n++] = i;
+
+        for (int i = 0; i < n; i++) {
+            int d = q[i] % 10;
+            if (d < 9) q[n++] = q[i] * 10 + d + 1;
+        }
+
+        return 0;
+    }();
+
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> memo;
-        for(int i=1;i<=9;++i){
-            int n = i;
-            for(int j=n+1;j<=9;++j){
-                n=n*10 + j;
-                memo.push_back(n);
-            }
-        }    
-        sort(memo.begin(),memo.end());
-        vector<int> ans;
-        for(auto& a:memo)
-            if(a>=low && a<=high)   ans.push_back(a);
-        return ans;
+        vector<int> res;
+
+        for (auto& x : q)
+            if (x >= low && x <= high)
+                res.push_back(x);
+
+        return res;
     }
 };
