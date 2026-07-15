654ddab80b7f56ccd51b72a51cfeafc0f045d848
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-15 13:53:57 +0530

    2 ms (18.99%) 3658-gcd-of-odd-and-even-sums .cpp

--- LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ LeetCode/Easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumodd=0,sumeven=0;
-        int odd= 1;
-        int even=2;
-        for(int i=0;i<n;i++){
-            sumodd+=odd;
-            sumeven+=even;
-            odd+=2;
-            even+=2;
-        }
-        return gcd(sumodd,sumeven);
+        return n;
     }
 };
