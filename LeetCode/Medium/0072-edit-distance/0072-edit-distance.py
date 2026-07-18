ad3bc10321c59b726638f496eed4a85414609a6a
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-18 12:35:06 +0530

    55 ms (28.67%) 0072-edit-distance .py

---
+++ LeetCode/Medium/0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDistance(self, w1: str, w2: str) -> int:
+        n = len(w1)
+        m = len(w2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        for i in range(m+1):
+            dp[0][i] = i
+        for i in range(n+1):
+            dp[i][0] = i
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if w1[i-1] == w2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1]) + 1
+        return dp[n][m]
