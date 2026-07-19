c5188e2f692944ef293b10c6dcc73cab204ecf0d
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-19 15:21:56 +0530

    0 ms (100%) 1081-smallest-subsequence-of-distinct-characters .cpp

--- LeetCode/Medium/1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ LeetCode/Medium/1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        int freq[27] = {0};
-        bitset<27> seen;
-        string stack;
-        for (auto& c : s)
-            freq[c & 31]++;
-        for (auto& c : s) {
-            int x = c & 31;
-            freq[x]--;
-            if (seen.test(x))
-                continue;
-            while (stack.length() && stack.back() > c && freq[stack.back() & 31]) {
-                seen.reset(stack.back() & 31);
-                stack.pop_back();
-            }
-            stack.push_back(c);
-            seen.set(x);
+        vector<int> vis(26), num(26);
+        for (char ch : s) {
+            num[ch - 'a']++;
         }
-        return stack;
+
+        string stk;
+        for (char ch : s) {
+            if (!vis[ch - 'a']) {
+                while (!stk.empty() && stk.back() > ch) {
+                    if (num[stk.back() - 'a'] > 0) {
+                        vis[stk.back() - 'a'] = 0;
+                        stk.pop_back();
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = 1;
+                stk.push_back(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return stk;
     }
 };
