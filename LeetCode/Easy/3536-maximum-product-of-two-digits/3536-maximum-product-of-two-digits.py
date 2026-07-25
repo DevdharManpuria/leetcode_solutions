d4b3d20fad29e770a9f8f69f8e7e26864ce3c943
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-25 15:07:28 +0530

    0 ms (100%) 3536-maximum-product-of-two-digits .py

--- LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -1,3 +1,4 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-        return int(s[-1]) * int(s[-2]) if len(s := sorted(str(n))) >= 2 else 0
+        return int(sorted(str(n))[-1]) * int(sorted(str(n))[-2]) 
+        
