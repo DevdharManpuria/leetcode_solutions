ca7230dd015cf8025a984fda5f1dad5e05b696a7
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-25 14:58:00 +0530

    0 ms (100%) 3536-maximum-product-of-two-digits .cpp

--- LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ LeetCode/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        string s = to_string(n);
-        sort(s.begin(),s.end());
-        return (s[s.size()-1] - '0') * (s[s.size()-2] - '0');
+        int s = 0,ss = 0;
+        while(n>0){
+            int d = n%10;
+            if(d > s){
+                ss = s;
+                s = d;
+            }
+            else if(d <= s && d > ss) ss = d;
+            n=n/10;
+        }
+        return s*ss;
     }
 };
