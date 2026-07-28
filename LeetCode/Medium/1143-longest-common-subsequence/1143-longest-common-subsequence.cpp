df05e0d066d549afefdccfd99773861931845713
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-07-28 11:38:49 +0530

    11 ms (98.7%) 1143-longest-common-subsequence .cpp

---
+++ LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        if(text1.size()<text2.size())
+            swap(text1,text2);
+        vector<int> prev(text2.size()+1,0);
+        vector<int> curr(text2.size()+1,0);
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1] == text2[j-1])
+                    curr[j] = 1 + prev[j-1];
+                else
+                    curr[j] = max(curr[j-1],prev[j]);
+            }
+            swap(prev,curr);
+        }
+        return prev[text2.size()];
+    }
+};
