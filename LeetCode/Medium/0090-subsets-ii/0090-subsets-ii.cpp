4d4d1917325f1e6dcd3231de5d4176f6f88b2537
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-15 19:57:46 +0530

    0 ms (100%) 0090-subsets-ii .cpp

--- LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.cpp
+++ LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        curr(nums,0,{});
-        return ans;
-    }
-    void curr(vector<int>& nums,int idx,vector<int> temp){
-        ans.push_back(temp);
-        for(int i=idx;i<nums.size();i++){
-            if(i>idx && nums[i] == nums[i-1])
-                continue;
-            temp.push_back(nums[i]);
-            curr(nums,i+1,temp);
-            temp.pop_back();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res = {{}};
+        int prevIdx = 0;
+        int idx = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            idx = (i >= 1 && nums[i] == nums[i - 1]) ? prevIdx : 0;
+            prevIdx = res.size();
+            for (int j = idx; j < prevIdx; j++) {
+                std::vector<int> tmp = res[j];
+                tmp.push_back(nums[i]);
+                res.push_back(tmp);
+            }
         }
+        return res;
     }
 };
