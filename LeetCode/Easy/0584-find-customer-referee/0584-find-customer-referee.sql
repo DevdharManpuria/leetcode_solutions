c128dbd6a934df226e1c5fb313e244c9c2763df6
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-16 00:21:22 +0530

    554 ms (34.56%) 0584-find-customer-referee .sql

--- LeetCode/Easy/0584-find-customer-referee/0584-find-customer-referee.sql
+++ LeetCode/Easy/0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,7 +1,1 @@
-# Write your MySQL query statement below
-SELECT 
-    name
-FROM 
-    Customer
-WHERE 
-    referee_id  IS NULL OR referee_id <> 2
+Select name from Customer where referee_id != 2 or referee_id is null
