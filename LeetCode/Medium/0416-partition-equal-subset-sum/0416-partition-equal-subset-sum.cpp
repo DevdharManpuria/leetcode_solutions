23e0b5f12a42b968f3a15e86f420afd589d8bef9
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-15 21:10:13 +0530

    64 ms (87.62%) 0416-partition-equal-subset-sum .cpp

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,17 +4,15 @@ public:
         if (sum(nums) % 2 != 0) {
             return false;
         }
-
         int target = sum(nums) / 2;
         vector<bool> dp(target + 1, false);
-
         dp[0] = true;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = target; j >= nums[i]; j--) {
                 dp[j] = dp[j] || dp[j - nums[i]];
+                if(dp[target]==true) return true;
             }
         }
-
         return dp[target];
     }
 
