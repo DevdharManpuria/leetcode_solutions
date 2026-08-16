83de194d574fab60d19325a03517d54cd23d2b00
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-16 15:34:14 +0530

    36 ms (98.47%) 2029-stone-game-ix .py

--- LeetCode/Medium/2029-stone-game-ix/2029-stone-game-ix.py
+++ LeetCode/Medium/2029-stone-game-ix/2029-stone-game-ix.py
@@ -3,6 +3,4 @@ class Solution:
         f = [0,0,0]
         for i in stones:
             f[i%3]+=1
-        if f[0]%2==0:
-            return f[1]>0 and f[2]>0
-        return abs(f[2]-f[1]) > 2
+        return (f[1] > 0 and f[2] > 0) if f[0]%2==0 else abs(f[2]-f[1]) > 2
