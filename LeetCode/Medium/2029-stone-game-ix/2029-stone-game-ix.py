dff92401ebeac33b25f7890ded7dac1f3f5272a1
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-16 15:30:26 +0530

    43 ms (88.55%) 2029-stone-game-ix .py

---
+++ LeetCode/Medium/2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        f = [0,0,0]
+        for i in stones:
+            f[i%3]+=1
+        if f[0]%2==0:
+            return f[1]>0 and f[2]>0
+        return abs(f[2]-f[1]) > 2
