68b5db249407a0e57e15210bc17553993ad4dcb1
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-18 19:56:27 +0530

    0 ms (100%) 3471-find-the-largest-almost-missing-integer .cpp

--- LeetCode/Easy/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ LeetCode/Easy/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
+        if (k == nums.size()) return *max_element(nums.begin(), nums.end());
         unordered_map<int, int> mp;
-        for (int i : nums) mp[i]++;
-        int m = -1;
-        int mo = -1;
-        for (auto& it : mp) {
-            if (it.second == 1) m = max(m, it.first);
-            mo = max(mo, it.first);
+        for (int x : nums) mp[x]++;
+        if (k == 1) {
+            int ans = -1;
+            for (auto& [val, freq] : mp) {
+                if (freq == 1) ans = max(ans, val);
+            }
+            return ans;
         }
-        if (k == 1) return m;
-        if (k == nums.size()) return mo;
-        if (mp[nums[0]] > 1 && mp[nums[nums.size() - 1]] > 1) 
-            return -1;
-        else if (mp[nums[0]] > 1 && mp[nums[nums.size() - 1]] == 1) 
-            return nums[nums.size() - 1];
-        else if (mp[nums[0]] == 1 && mp[nums[nums.size() - 1]] > 1) 
-            return nums[0];
-        else 
-            return max(nums[0], nums[nums.size() - 1]);
+        int first = (mp[nums.front()] == 1) ? nums.front() : -1;
+        int last = (mp[nums.back()] == 1) ? nums.back() : -1;
+        return max(first, last);
     }
 };
