e06e8c59b6457f24c09c36abf464e7e29ff0d085
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-20 01:03:04 +0530

    3 ms (66.5%) 0001-two-sum .cpp

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -3,10 +3,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> mp;
         for(int i=0;i<nums.size();i++){
-            if(mp.find(target-nums[i])!=mp.end()){
-                return{mp[target-nums[i]],i};
-            }
-            mp[nums[i]] = i;
+            if(mp.find(target-nums[i]) != mp.end()) return {mp[target-nums[i]],i};
+            mp.insert({nums[i],i});
         }
         return {};
     }
