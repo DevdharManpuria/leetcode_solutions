8aca1c88c2c8351379a35db4351123e029b0aa6b
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-08-25 11:29:24 +0530

    0 ms (100%) 3718-smallest-missing-multiple-of-k .cpp

--- LeetCode/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ LeetCode/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> seen(nums.begin(),nums.end());
-        int ans=k;
-        while(seen.count(ans)) ans+=k;
-        return ans;
+        uint64_t x[2] = {0, 0};
+        for (auto& n : nums)
+            if (n % k == 0) {
+                int i = n / k - 1;
+                x[i >> 6] |= 1ULL << (i & 63);
+            }
+
+        int z = x[0] == -1ULL;
+        return (z * 64 + bit_width(++x[z] & -x[z])) * k;
     }
 };
