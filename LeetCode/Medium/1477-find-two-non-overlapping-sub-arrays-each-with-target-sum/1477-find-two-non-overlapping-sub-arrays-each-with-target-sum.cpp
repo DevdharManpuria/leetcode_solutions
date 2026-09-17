a026ac3b30726652ab0616dbd3ffff7d0b68375d
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-09-17 11:20:06 +0530

    4 ms (88.92%) 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum .cpp

--- LeetCode/Medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ LeetCode/Medium/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int minSumOfLengths(vector<int>& arr, int target) {
-        int n = arr.size();
-        int ans = n+1;
-        int s = 0;
-        vector<int> dp(n+1,n);
-        for(int l=0,r=0;r<n;r++){
-            s+=arr[r];
-            while(s>target) s-=arr[l++];
-            dp[r+1] = dp[r];
-            if(s==target){
-                ans = min(ans,r-l+1+dp[l]);
-                dp[r+1] = min(dp[r],r-l+1);
+    int minSumOfLengths(vector<int>& A, int k) {
+        int n = A.size();
+        int res = n + 1, sum = 0, i = 0;
+        vector<int> dp(n + 1, n);
+        for (int j = 0; j < n; j++) {
+            sum += A[j];
+            while (sum > k)
+                sum -= A[i++];
+            dp[j + 1] = dp[j];
+            if (sum == k) {
+                res = min(res, j - i + 1 + dp[i]);
+                dp[j + 1] = min(dp[j], j - i + 1);
             }
         }
-        return ans == n+1 ? -1 : ans;
+        return res == n + 1 ? -1 : res;
     }
 };
