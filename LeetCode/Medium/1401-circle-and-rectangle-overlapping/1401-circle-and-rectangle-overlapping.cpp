02e6f6ceb80265a42b8e7c4e3389eb6c1a0c5e38
Devdhar Manpuria <manpuriadevdhar95@gmail.com>
2026-09-19 15:47:05 +0530

    0 ms (100%) 1401-circle-and-rectangle-overlapping .cpp

---
+++ LeetCode/Medium/1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        long long di = 0;
+        if(xCenter < x1 || xCenter > x2)
+            di += min(pow(x1-xCenter,2), pow(x2-xCenter,2));
+        if(yCenter < y1 || yCenter > y2)
+            di += min(pow(y1-yCenter,2), pow(y2-yCenter,2));
+        return di <= radius*radius;
+    }
+};
